cc6de1014fe67b20148ab76fd435468a2a06b97d
Marcel van Duijn <epicmars264@gmail.com>
2021-09-01 12:01:22 +0200

    Update Movement + Solar system

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,15 +7,14 @@ public class Movement : MonoBehaviour
 
     [SerializeField] private float _RotationSpeed;
     [SerializeField] private float _Speed;
-
-
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float _IdleSpeed;
 
     void Update()
     {
+
+        float rotateinput = -Input.GetAxis("Horizontal") * _RotationSpeed * Time.deltaTime;
+        float forwardspeed = -Input.GetAxis("Vertical") * _Speed * Time.deltaTime;
         
+        transform.Rotate(0, forwardspeed + -_IdleSpeed, rotateinput);
     }
 }
