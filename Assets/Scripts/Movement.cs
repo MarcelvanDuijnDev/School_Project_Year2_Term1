7c2a44b071316615804312c85bf26ea78a8c32f6
Marcel van Duijn <epicmars264@gmail.com>
2021-09-01 11:14:03 +0200

    Update solar system

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    [SerializeField] private float _RotationSpeed;
+    [SerializeField] private float _Speed;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
