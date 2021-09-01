25d792eee937c19ebef9cf8cdb869421eac04768
Marcel van Duijn <epicmars264@gmail.com>
2021-09-01 13:21:04 +0200

    Scene changes

--- Assets/Scripts/DebrisCollector.cs
+++ Assets/Scripts/DebrisCollector.cs
@@ -4,25 +4,26 @@ using UnityEngine;
 
 public class DebrisCollector : MonoBehaviour
 {
-    [SerializeField] private float _CollectRange;
+    [SerializeField] private float _CollectRange = 5;
+    [SerializeField] private float _CollectSpeed;
     [SerializeField] private LayerMask _DebrisLayer;
 
-    [SerializeField] private List<Transform> _ObjectsInRange = new List<Transform>();
-
-
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Collider[] _ObjectsInRange;
 
 
     void Update()
     {
+        _ObjectsInRange = Physics.OverlapSphere(transform.position, _CollectRange, _DebrisLayer);
 
+        for (int i = 0; i < _ObjectsInRange.Length; i++)
+        {
+            _ObjectsInRange[i].transform.position = Vector3.MoveTowards(_ObjectsInRange[i].transform.position, transform.position, _CollectSpeed) * Time.deltaTime;
+        }
     }
 
     private void OnDrawGizmosSelected()
     {
+        Gizmos.color = new Vector4(0, 1, 0, 0.1f);
         Gizmos.DrawSphere(transform.position,_CollectRange);
     }
 }
